ffa16ce43c9d77589ad6107741231f3c4fbc8024
S3TN4s <juninhojuninhoats@gmail.com>
2025-06-30 19:37:55 -0300

    30/06/2025

--- Aula05/Aula05/Controllers/CustomerController.cs
+++ Aula05/Aula05/Controllers/CustomerController.cs
@@ -51,24 +51,39 @@ namespace Aula05.Controllers
             string fileContent = string.Empty;
             foreach (Customer c in CustomerData.Customers)
             {
-                fileContent +=
-                    String.Format("{0:5}", c.Id) +
-                    String.Format("{0:5}", c.Name) +
-                    String.Format("{0:64}", c.HomeAdress!.Id) +
-                    String.Format("{0:5}", c.HomeAdress!.City) +
-                    String.Format("{0:32}", c.HomeAdress!.State) +
-                    String.Format("{0:2}", c.HomeAdress!.Country) +
-                    String.Format("{0:32}", c.HomeAdress!.Street1) +
-                    String.Format("{0:64}", c.HomeAdress!.Street2) +
-                    String.Format("{0:64}", c.HomeAdress!.PostalCode) +
-                    String.Format("{0:9}", c.HomeAdress!.AdressType) ;
+                fileContent += 
+                    $"{c.Id};{c.Name};{c.HomeAddress!.Id};{c.HomeAddress.City};{c.HomeAddress.State};{c.HomeAddress.Country};{c.HomeAddress.Street1};{c.HomeAddress.Street2};{c.HomeAddress.PostalCode};{c.HomeAddress.AddressType}\n";
             }
 
-            SaveFile(fileContent, "DelimitadedFile.txt");
+            SaveFile(fileContent, "DelimitatedFile.txt");
 
             return View();
         }
 
+        [HttpGet]
+        public IActionResult ExportFixedFile()
+        {
+            string fileContent = string.Empty;
+            foreach (Customer c in CustomerData.Customers)
+            {
+                fileContent +=
+                    String.Format("{0:5}{1:64}", c.Id, c.Name) +                    
+                    String.Format("{0:5}", c.HomeAddress!.Id) + 
+                    String.Format("{0:32}", c.HomeAddress!.City) +
+                    String.Format("{0:2}", c.HomeAddress!.State) +
+                    String.Format("{0:32}", c.HomeAddress!.Country) +
+                    String.Format("{0:64}", c.HomeAddress!.Street1) +
+                    String.Format("{0:64}", c.HomeAddress!.Street2) +
+                    String.Format("{0:9}", c.HomeAddress!.PostalCode) +
+                    String.Format("{0:16}", c.HomeAddress!.AddressType) + 
+                    "\n";
+
+            }
+
+            SaveFile(fileContent, "FixedFile.txt");
+
+            return RedirectToAction("Index");
+        }
 
         [HttpGet]
         public IActionResult Delete(int? id)
@@ -76,25 +91,23 @@ namespace Aula05.Controllers
             if (id is null || id.Value <= 0)
                 return NotFound();
 
-            Customer customer = _customerRepository.Retrieve(id.Value);
+            Customer customer = 
+                _customerRepository.Retrieve(id.Value);
 
             if (customer == null)
                 return NotFound();
 
             return View(customer);
-
         }
 
-        [HttpGet]
+        [HttpPost]
         public IActionResult ConfirmDelete(int? id)
         {
             if (id is null || id.Value <= 0)
                 return NotFound();
 
-            if (
-                _customerRepository.DeleteById(id.Value))
+            if (!_customerRepository.DeleteById(id.Value))
                 return NotFound();
-
 
             return RedirectToAction("Index");
         }
@@ -122,29 +135,23 @@ namespace Aula05.Controllers
                     fileName
                 );
 
-                if (!System.IO.File.Exists(filepath))
+                using (StreamWriter sw = System.IO.File.CreateText(filepath))
                 {
-                    using (StreamWriter sw = System.IO.File.CreateText(filepath))
-                    {
-                        sw.Write(content);
-                    }
-
+                    sw.Write(content);
                 }
             }
-
             catch (IOException ioEx)
             {
                 string msg = ioEx.Message;
-                ret = false;
-                //throw ioEx;
+                ret = false;                
             }
             catch (Exception ex)
             {
                 string msg = ex.Message;
                 ret = false;
             }
+
             return ret;
-        }
+        }    
     }
 }
-
--- Aula05/Repository/OrderRepository.cs
+++ Aula05/Repository/OrderRepository.cs
@@ -1,25 +1,70 @@
-﻿using System;
+﻿using Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Modelo;
 
 namespace Repository
 {
-    internal class OrderRepository
+    public class OrderRepository
     {
-        public Order Retrieve(int orderId)
+        public Order Retrieve(int id)
         {
-            return new Order();
+            foreach (Order c in CustomerData.Orders)
+                if (c.Id == id)
+                    return c;
+
+            return null!;
         }
-        public List<Order> Retrieve()
+
+        public List<Order> RetrieveByName(string name)
         {
-            return new List<Order>();
+            List<Order> ret = [];
+
+            foreach (Order o in CustomerData.Orders)
+                if (o.Customer!.Name!.ToLower().Contains(name.ToLower()))
+                    ret.Add(o);
+
+            return ret;
         }
+
+        public List<Order> RetrieveAll()
+        {
+            return CustomerData.Orders;
+        }
+
         public void Save(Order order)
         {
-
+            order.Id = GetCount() + 1;
+            CustomerData.Orders.Add(order);
         }
+
+        public bool Delete(Order order)
+        {
+            return CustomerData.Orders.Remove(order);
+        }
+
+        public bool DeleteById(int id)
+        {
+            Order order = Retrieve(id);
+
+            if (order != null)
+                return Delete(order);
+
+            return false;
+        }
+
+        public void Update(Order newOrder)
+        {
+            Order oldOrder = Retrieve(newOrder.Id);
+            oldOrder.Id = newOrder.Id;
+            oldOrder.Customer = newOrder.Customer;
+            oldOrder.OrderDate = newOrder.OrderDate;
+            oldOrder.ShippingAddress = newOrder.ShippingAddress;
+            oldOrder.OrderItems = newOrder.OrderItems;
+        }
+
+        public int GetCount() => CustomerData.Orders.Count;
     }
 }
